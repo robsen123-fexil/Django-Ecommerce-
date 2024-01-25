0843882d3e5be240083c344cb4a169a5b3d8ad0c
Robsen Teshome <111446547+robsen123-fexil@users.noreply.github.com>
2024-01-25 15:43:19 +0300

    Add files via upload

---
+++ DJ/core/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from .models import item , orderitem , order
+admin.site.register(item)
+admin.site.register(orderitem)
+admin.site.register(order)
---
+++ DJ/core/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.conf import settings
+class item(models.Model):
+    title=models.CharField(max_length=200)
+    price=models.FloatField()
+    def __str__(self):
+        return self.title
+class orderitem(models.Model):
+    item=models.ForeignKey(item, on_delete=models.CASCADE)    
+    
+    def __str__(self):
+        return self.title
+class order(models.Model):
+     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+     items = models.ManyToManyField(orderitem)
+     start_date=models.DateTimeField(auto_now_add=True)
+     ordered_date=models.DateTimeField()
+     ordered=models.BooleanField(default=False)
+     def __str__(self):
+         return self.user.username
+
+ 
+         
