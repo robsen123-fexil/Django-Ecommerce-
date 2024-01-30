ca1a31c23e1dff0e57d26fb8879ed0db06a79372
robsen123-fexil <robsenteshoma1@gmail.com>
2024-01-30 20:13:01 +0300

    slug url

--- DJ/core/models.py
+++ DJ/core/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.conf import settings
+from django.shortcuts import reverse
+from django.utils import timezone
 CATEGORIES_CHOICE=(('S', 'shirts'), 
                    ('SW', 'sportshirt') , 
                    ('OW','outwear')
@@ -15,6 +17,13 @@ class items(models.Model):
     price=models.FloatField()
     categories=models.CharField(choices=CATEGORIES_CHOICE, max_length=2)
     label=models.CharField(choices=LABEL_CHOICES, max_length=1)
+    slug=models.SlugField(default=timezone.now)
+    def __str__(self):
+        return self.title
+    def get_absolute_url(self):
+        return reverse('core:product', kwarg={
+            'slug':self.slug
+        })
     
 class orderitem(models.Model):
     item=models.ForeignKey(items, on_delete=models.CASCADE)    
