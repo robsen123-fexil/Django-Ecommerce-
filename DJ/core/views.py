8071c7ecf133b64b0c1ae7dc981759e359552fef
robsen123-fexil <robsenteshoma1@gmail.com>
2024-02-17 10:34:35 +0300

    meassage info

--- DJ/core/views.py
+++ DJ/core/views.py
@@ -3,6 +3,8 @@ from django.views.generic import ListView,DetailView
 from .models import items , orderitem , order
 from django.shortcuts import redirect
 from django.utils import timezone
+from django.shortcuts import get_object_or_404, redirect
+from .models import items, order, orderitem
 from django.contrib import messages
 class products(ListView):
     model=items
@@ -23,27 +25,27 @@ class homeview(ListView):
     template_name='home.html'
 def add_to_cart(request, slug):
     item = get_object_or_404(items, slug=slug)
-    order_item= orderitem.objects.get_or_create(item=item , user=request.user, ordered=False)
-    order_qs = order.objects.filter(user=request.user, ordered=False)
-
+    order_item, created = orderitem.objects.get_or_create(item=item , user=request.user, ordered=False)
+    
+    
+    order_qs=order.objects.filter(user=request.user ,ordered=False)
     if order_qs.exists():
         orderuser = order_qs[0]
         if orderuser.items.filter(item__slug=item.slug).exists():
             order_item.quality += 1
             order_item.save()
-            messages.info(request,"THIS PRODUCT IS UPDATED")
+            messages.info(request, "THE PRODUCT IS  UPDATED ")
         else:
-            messages.info(request, "THIS ITEM IS ALREADY ADDED TO YOUR CART ")
+            messages.info(request, "THE PRODUCT HAVE BEEN ADDED TO YOUR CART ")
             orderuser.items.add(order_item)
+            messages.info(request, "THE PRODUCT HAVE BEEN ADDED TO YOUR CART")
     else:
         order_date = timezone.now()
         neworder = order.objects.create(user=request.user, ordered_date=order_date)
         neworder.items.add(order_item)
-        messages.info(request, "THE ITEM IS ADDED TO YOU CART ")
-        
+        messages.info(request, "THE PRODUCT HAVE BEEN ADDED TO YOUR CART  ")
     return redirect("core:product", slug=slug)
-from django.shortcuts import get_object_or_404, redirect
-from .models import items, order, orderitem
+
 
 def remove_from_cart(request, slug):
     item = get_object_or_404(items, slug=slug)
@@ -56,17 +58,17 @@ def remove_from_cart(request, slug):
             order_item = orderitem.objects.filter(item=item, user=request.user, ordered=False).first()
             order_item.quality -= 1
             order_item.save()
-            messages.info(request , "THE PRODUCT IS UPDATED ")
+            messages.info(request, "THE PRODUCT IS REMOVED FROM YOUR CART ")
             if order_item:
                 orderuser.items.remove(order_item)
-                messages.info(request , "THE PRODUCT IS REMOVED SUCCESSFULLY")
-
+                messages.info(request, "THE PRODUCT WAS NOT ON YOUR Cart")
                 return redirect("core:product", slug=slug)
         else:
-             messages.info(request , "THE PRODUCT IS NOT IN YOUR CART")
+             messages.info(request, "THE PRODUCT was not ON YOUR CART ")
              redirect("core:product", slug=slug)
+             messages.info(request, "THE PRODUCT IS REMOVED FROM YOUR CART ")
     else:
-         messages.info(request, "YOUR DONT HAVE ACTIVATED CARTS")
+         messages.info(request,"THE PRODUCT IS NOT ON YOUR CART ")
          redirect("core:product", slug=slug)
 
     return redirect("core:product", slug=slug)
