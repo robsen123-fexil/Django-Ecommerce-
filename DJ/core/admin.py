07c10a8eb810ed732810d8f923fcc31c3f64e83e
robsen123-fexil <robsenteshoma1@gmail.com>
2024-02-22 17:49:10 +0300

    url update

--- DJ/core/admin.py
+++ DJ/core/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
 from .models import items , orderitem , order
+
+class orderadmin(admin.ModelAdmin):
+    list_display=[ 'user', 'ordered']
+
+
 admin.site.register(items)
 admin.site.register(orderitem)
-admin.site.register(order)
+admin.site.register(order, orderadmin)
